f6d7882c9d7e83dd7f3f23f0a1806fccece0a48f
syrkles <syrkles@gmail.com>
2018-08-05 06:58:39 +0000

    fix building derp

--- aoscp.mk
+++ aoscp.mk
@@ -22,7 +22,7 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/aoscp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
 
 # Inherit from whyred device
